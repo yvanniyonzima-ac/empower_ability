9b67cea3ff9c4002c4fe6c27533c7a0adacca18d
Yvan Niyonzima <yvanniyonzima@Yvans-MacBook-Air.local>
2025-03-30 23:21:13 -0400

    initial template push

---
+++ empower_ability_lab.js
@@ -0,0 +1,11 @@
+//Template Function that can be used to run JavaScript on the page
+//Note: This can be changed to whatever JavaScript formatting you would like
+function knowledgeRunner(){
+
+}
+
+
+
+
+
+knowledgeRunner()
